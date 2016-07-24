64a4adf3789adc7b8be9b8809d578fbab4354562
Matias Insaurralde <matias@insaurral.de>
2016-07-24 14:57:10 -0400

    Adding C# and VB sample code

---
+++ examples/HelloWorld.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HelloWorld {
+
+public class HelloWorld {
+    public static void Hello() {
+      Console.WriteLine("hello from C#");
+    }
+}
+
+}
---
+++ examples/HelloWorld.vb
@@ -0,0 +1,7 @@
+Namespace HelloWorld
+  Public Class HelloWorld
+    Shared Sub hello()
+      Console.WriteLine("Hello from VB.NET")
+    End Sub
+  End Class
+End Namespace
