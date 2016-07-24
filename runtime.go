ff22f53649e1a364dc9c72e2f3628de4cb1eb4af
Matias Insaurralde <matias@insaurral.de>
2016-07-24 10:01:04 -0400

    gofmt everything

--- runtime.go
+++ runtime.go
@@ -11,108 +11,108 @@ static void abc() {
 */
 import "C"
 
-import(
-  "github.com/kardianos/osext"
+import (
+	"github.com/kardianos/osext"
 
-  "unsafe"
-  "errors"
-  "strings"
+	"errors"
+	"strings"
+	"unsafe"
 )
 
 type Runtime struct {
-  Params RuntimeParams
+	Params RuntimeParams
 }
 
 type RuntimeParams struct {
-  ExePath string
-  AppDomainFriendlyName string
-  Properties map[string]string
-  ManagedAssemblyAbsolutePath string
+	ExePath                     string
+	AppDomainFriendlyName       string
+	Properties                  map[string]string
+	ManagedAssemblyAbsolutePath string
 }
 
 const DefaultAppDomainFriendlyName string = "app"
 
 func NewRuntime(params RuntimeParams) (err error, runtime Runtime) {
-  runtime = Runtime{Params: params}
-  // C.initializeCoreCLR()
-  // C.executeAssembly()
+	runtime = Runtime{Params: params}
+	// C.initializeCoreCLR()
+	// C.executeAssembly()
 
-  err = runtime.Init()
+	err = runtime.Init()
 
-  C.abc()
+	C.abc()
 
-  return err, runtime
+	return err, runtime
 }
 
-func( r *Runtime ) Init() (err error) {
-  if r.Params.ExePath == "" {
-    r.Params.ExePath, err = osext.Executable()
-  }
-  if r.Params.AppDomainFriendlyName == "" {
-    r.Params.AppDomainFriendlyName = DefaultAppDomainFriendlyName
-  }
+func (r *Runtime) Init() (err error) {
+	if r.Params.ExePath == "" {
+		r.Params.ExePath, err = osext.Executable()
+	}
+	if r.Params.AppDomainFriendlyName == "" {
+		r.Params.AppDomainFriendlyName = DefaultAppDomainFriendlyName
+	}
 
-  propertyCount := len(r.Params.Properties)
+	propertyCount := len(r.Params.Properties)
 
-  propertyKeys := make([]string, 0, len(r.Params.Properties))
-  propertyValues := make([]string, 0, len(r.Params.Properties))
+	propertyKeys := make([]string, 0, len(r.Params.Properties))
+	propertyValues := make([]string, 0, len(r.Params.Properties))
 
-  for k, v := range r.Params.Properties {
-    propertyKeys = append(propertyKeys, k)
-    propertyValues = append(propertyValues, v)
-  }
+	for k, v := range r.Params.Properties {
+		propertyKeys = append(propertyKeys, k)
+		propertyValues = append(propertyValues, v)
+	}
 
-  ExePath := C.CString(r.Params.ExePath)
-  AppDomainFriendlyName := C.CString(r.Params.AppDomainFriendlyName)
-  PropertyCount := C.int(propertyCount)
-  PropertyKeys := C.CString(strings.Join(propertyKeys, ";"))
-  PropertyValues := C.CString(strings.Join(propertyValues, ";"))
+	ExePath := C.CString(r.Params.ExePath)
+	AppDomainFriendlyName := C.CString(r.Params.AppDomainFriendlyName)
+	PropertyCount := C.int(propertyCount)
+	PropertyKeys := C.CString(strings.Join(propertyKeys, ";"))
+	PropertyValues := C.CString(strings.Join(propertyValues, ";"))
 
-  CLRFilesAbsolutePath := C.CString("/usr/local/share/dotnet/shared/Microsoft.NETCore.App/1.0.0")
+	CLRFilesAbsolutePath := C.CString("/usr/local/share/dotnet/shared/Microsoft.NETCore.App/1.0.0")
 
-  ManagedAssemblyAbsolutePath := C.CString(r.Params.ManagedAssemblyAbsolutePath)
+	ManagedAssemblyAbsolutePath := C.CString(r.Params.ManagedAssemblyAbsolutePath)
 
-  var result C.int
-  result = C.initializeCoreCLR(ExePath, AppDomainFriendlyName, PropertyCount, PropertyKeys, PropertyValues, ManagedAssemblyAbsolutePath, CLRFilesAbsolutePath)
+	var result C.int
+	result = C.initializeCoreCLR(ExePath, AppDomainFriendlyName, PropertyCount, PropertyKeys, PropertyValues, ManagedAssemblyAbsolutePath, CLRFilesAbsolutePath)
 
-  if result == -1 {
-    err = errors.New("Runtime error")
-  }
+	if result == -1 {
+		err = errors.New("Runtime error")
+	}
 
-  C.free(unsafe.Pointer(ExePath))
-  C.free(unsafe.Pointer(AppDomainFriendlyName))
-  C.free(unsafe.Pointer(PropertyKeys))
-  C.free(unsafe.Pointer(PropertyValues))
-  C.free(unsafe.Pointer(ManagedAssemblyAbsolutePath))
-  C.free(unsafe.Pointer(CLRFilesAbsolutePath))
+	C.free(unsafe.Pointer(ExePath))
+	C.free(unsafe.Pointer(AppDomainFriendlyName))
+	C.free(unsafe.Pointer(PropertyKeys))
+	C.free(unsafe.Pointer(PropertyValues))
+	C.free(unsafe.Pointer(ManagedAssemblyAbsolutePath))
+	C.free(unsafe.Pointer(CLRFilesAbsolutePath))
 
-  return err
+	return err
 }
 
-func( r *Runtime ) Shutdown() (err error) {
-  var result C.int
-  result = C.shutdownCoreCLR()
+func (r *Runtime) Shutdown() (err error) {
+	var result C.int
+	result = C.shutdownCoreCLR()
 
-  if result == -1 {
-    err = errors.New("Shutdown error.")
-  }
+	if result == -1 {
+		err = errors.New("Shutdown error.")
+	}
 
-  return err
+	return err
 }
 
-func( r *Runtime) ExecuteManagedAssembly(assembly string) (err error) {
-  var result C.int
-  CAssembly := C.CString(assembly)
-  result = C.executeManagedAssembly(CAssembly)
-  C.free(unsafe.Pointer(CAssembly))
+func (r *Runtime) ExecuteManagedAssembly(assembly string) (err error) {
+	var result C.int
+	CAssembly := C.CString(assembly)
+	result = C.executeManagedAssembly(CAssembly)
+	C.free(unsafe.Pointer(CAssembly))
 
-  if result == -1 {
-    err = errors.New("Can't execute")
-  }
+	if result == -1 {
+		err = errors.New("Can't execute")
+	}
 
-  return err
+	return err
 }
 
-func( r *Runtime) CreateDelegate() {
-  C.createDelegate()
+func (r *Runtime) CreateDelegate() {
+	C.createDelegate()
 }
