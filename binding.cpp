ff844ea6ecb2de4efae981462678a25ce5c7e64f
Matias Insaurralde <matias@insaurral.de>
2016-07-24 07:40:31 -0400

    Basic C interface

---
+++ binding.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "binding.hpp"
+
+void initializeCoreCLR(const char* exePath,
+            const char* appDomainFriendlyName,
+            int propertyCount,
+            const char* propertyKeys,
+            const char* propertyValues) {
+  printf("initializeCoreCLR()\n");
+
+
+}
+
+void executeAssembly() {
+  printf("executeAssembly()\n");
+}
