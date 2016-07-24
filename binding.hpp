a407d8f6486ba4bf52ebeade89aa3509525ba8a5
Matias Insaurralde <matias@insaurral.de>
2016-07-24 09:38:46 -0400

    Returning ints for error handling

--- binding.hpp
+++ binding.hpp
@@ -1,11 +1,24 @@
 #ifdef __cplusplus
+
+#include "coreclrhost.h"
+#ifndef SUCCEEDED
+#define SUCCEEDED(Status) ((Status) >= 0)
+#endif // !SUCCEEDED
+
+void* hostHandle;
+unsigned int domainId;
+
 extern "C" {
 #endif
-void initializeCoreCLR(const char* exePath,
+int initializeCoreCLR(const char* exePath,
             const char* appDomainFriendlyName,
             int propertyCount,
-            const char* propertyKeys,
-            const char* propertyValues);
+            const char* mergedPropertyKeys,
+            const char* mergedPropertyValues,
+            const char* managedAssemblyAbsolutePath,
+            const char* clrFilesAbsolutePath);
+int shutdownCoreCLR();
+void parseValues(const char*, char**, int);
 void executeAssembly();
 #ifdef __cplusplus
 }
