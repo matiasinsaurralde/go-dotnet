13334a56b1160ac952afc7a93c0a494be16273e7
Matias Insaurralde <matias@insaurral.de>
2016-07-31 14:49:31 -0400

    Push basic documentation

---
+++ doc.go
@@ -0,0 +1,12 @@
+// Package go-dotnet provides a very simple wrapper for the .NET Core CLR.
+//
+// You may find more information about .NET Core here:
+//
+//    https://dotnet.github.io/
+//
+// This project makes use of the Hosting APIs:
+//
+//    https://msdn.microsoft.com/en-us/library/ms404385(v=vs.110).aspx
+//
+
+package dotnet
