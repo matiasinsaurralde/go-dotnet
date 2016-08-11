2afc8556e2f0ac248277237e97ee7fafae805515
Matias Insaurralde <matias@insaurral.de>
2016-08-11 01:19:32 -0400

    Playground programs

---
+++ generator/playground/other_struct.go
@@ -0,0 +1,7 @@
+package mypackage
+
+//go:generate env
+
+type OtherStruct struct {
+
+}
---
+++ generator/playground/sample.go
@@ -0,0 +1,17 @@
+package mypackage
+
+//create_delegate: HelloWorld.Hello(string) string
+func SayHello(string) (string) {
+}
+
+//create_delegate: HelloWorld.Goodbye(string) string
+func SayGoodbye(string, int) (string, int) {
+}
+
+//create_delegate: HelloWorld.GetInt() int
+func GetInt() (int) {
+}
+
+//create_delegate: HelloWorld.GetInt(int)
+func SetInt(int) () {
+}
