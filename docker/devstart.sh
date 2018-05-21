c3f0c950f6ac3085c06735b0e2353aef238e6652
Matias Insaurralde <matias@insaurral.de>
2018-05-21 02:07:23 -0400

    Remove unused Docker scripts

--- docker/devstart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CONTAINER=go-dotnet
-WORKDIR=/go/src/github.com/matiasinsaurralde/go-dotnet
-
-docker build -t $CONTAINER .
-
-docker run -it --rm -v `pwd`/../:$WORKDIR -w $WORKDIR  $CONTAINER 
